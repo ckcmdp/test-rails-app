2490a3d8f53f88ffaf3f409b956515a472ccb024
ckcmdp <ckumar@kreeti.com>
2018-11-26 18:20:44 +0530

    capistrano config file changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,10 +21,10 @@ set :branch, :master
 set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, "config/database.yml"
+set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs,  %w{"log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"}
+set :linked_dirs,  %w{log tmp/pids tmp/cache tmp/sockets public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
